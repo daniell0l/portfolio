0d5da01aa5fc6319a7260bec021b48d11b3b464b
daniell0l <daniel_l0l@hotmail.com>
2023-07-06 03:20:19 -0300

    feat: arrumando JS para escrever e apagar a class digitando

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-function ativaLetra(elemento) {
-  const arrTexto = elemento.innerHTML.split('');
+function ativaLetra(elemento, texto) {
+  const arrTexto = texto.split('');
   elemento.innerHTML = '';
   let i = 0;
+  let escrevendo = true;
 
   function digitar() {
     setTimeout(() => {
-      elemento.innerHTML += arrTexto[i];
-      i++;
-
-      if (i < arrTexto.length) {
-        digitar();
-      } else {
-        setTimeout(() => {
-          elemento.innerHTML = '';
-          i = 0;
+      if (escrevendo) {
+        elemento.innerHTML += arrTexto[i];
+        i++;
+        if (i < arrTexto.length) {
           digitar();
-        }, 2000);
+        } else {
+          escrevendo = false;
+          setTimeout(apagar, 1000);
+        }
+      } else {
+        elemento.innerHTML = elemento.innerHTML.slice(0, -1);
+        if (elemento.innerHTML.length === 0) {
+          escrevendo = true;
+          if (texto === 'Web Developer') {
+            ativaLetra(elemento, 'Mobile Developer');
+          } else if (texto === 'Mobile Developer') {
+            ativaLetra(elemento, 'Web Developer');
+          }
+        } else {
+          setTimeout(apagar, 75);
+        }
       }
     }, 75);
+  }
+
+  function apagar() {
+    setTimeout(() => {
+      elemento.innerHTML = elemento.innerHTML.slice(0, -1);
+      if (elemento.innerHTML.length > 0) {
+        apagar();
+      } else {
+        escrevendo = true;
+        if (texto === 'Web Developer') {
+          ativaLetra(elemento, 'Mobile Developer');
+        } else if (texto === 'Mobile Developer') {
+          ativaLetra(elemento, 'Web Developer');
+        }
+      }
+    }, 30);
   }
 
   digitar();
 }
 
 const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+ativaLetra(titulo, 'Web Developer');
